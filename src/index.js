378fd6d1019b4bc4558af4be673bf23a1039b7f3
Kiran V <kiranvenugopal07@gmail.com>
2022-02-08 22:25:12 +0530

    initial

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
+
+function MainPage() {
+  return <div className="main-page">Hello World !!! </div>;
+}
+
+ReactDOM.render(<MainPage />, document.getElementById("root"));
